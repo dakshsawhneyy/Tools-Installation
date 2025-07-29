31bb87467d4d053bbb2d2c4db2ced0c02c9f2c92
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-07-29 19:17:16 +0530

    Update kafka.sh

--- Kafka/kafka.sh
+++ Kafka/kafka.sh
@@ -3,6 +3,7 @@ docker run -p 9092:9092 \
   -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://<your-wifi-ip>:9092 \   # Tells kafka on which port it is running
   -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \    # How many replications - 1 means No Replication
   -e KAFKA_PROCESS_ROLES=broker \    # Explicitly tells Kafka to run as a broker, not controller-only 
+  -e KAFKA_NODE_ID=1 \
   -e CLUSTER_ID=q1w2e3r4-t5y6-7u8i-9o0p-a1s2d3f4g5h6 \  
   confluentinc/cp-kafka
 
