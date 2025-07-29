e3aadb006152bfbe47eeb8dbef476128ab99c3da
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-07-29 19:38:14 +0530

    Update kafka.sh

--- Kafka/kafka.sh
+++ Kafka/kafka.sh
@@ -1,9 +1,16 @@
-docker run -p 9092:9092 \
-  -e KAFKA_ZOOKEEPER_CONNECT=<your-wifi-ip>:2181 \    # Tell the ip where zookeeper is running on your laptop
-  -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://<your-wifi-ip>:9092 \   # Tells kafka on which port it is running
-  -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \    # How many replications - 1 means No Replication
-  -e KAFKA_PROCESS_ROLES=broker \    # Explicitly tells Kafka to run as a broker, not controller-only 
+docker network create kafka-net
+
+docker run -d \
+  --name kafka \
+  --network kafka-net \
+  -p 9092:9092 \
+  -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 \
+  -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092 \
+  -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 \
+  -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \
+  -e KAFKA_PROCESS_ROLES=broker \
   confluentinc/cp-kafka:6.2.0
+
 
 
 # Kafka Install Using Yarn
