78dcf88c5d00893ec9404c788ac74871445d3019
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-07-31 15:50:08 +0530

    Create docker-compose.sh

---
+++ Docker-Compose/docker-compose.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
