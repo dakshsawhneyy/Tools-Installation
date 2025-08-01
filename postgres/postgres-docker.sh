a39108a5d27ed6905f9ac72d0495c3a3a6486351
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-01 17:23:51 +0530

    Update postgres-docker.sh

--- postgres/postgres-docker.sh
+++ postgres/postgres-docker.sh
@@ -1,9 +1,13 @@
+# in postgres, username is postgres not root
 docker run --name my_postgres \
   -e POSTGRES_USER=postgres \
-  -e POSTGRES_PASSWORD=mysecretpassword \
+  -e POSTGRES_PASSWORD=admin \
   -e POSTGRES_DB=mydb \
   -p 5432:5432 \
   -d postgres
 
 sudo apt update
 sudo apt install postgresql-client
+
+# Set Password
+psql -h localhost -p 5432 -U postgres -d mydb
