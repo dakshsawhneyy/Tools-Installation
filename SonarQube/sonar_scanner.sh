6efa35542080c79b35c5d69d1302d1288a00de90
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-04 09:19:15 +0530

    Create sonar_scanner.sh

---
+++ SonarQube/sonar_scanner.sh
@@ -0,0 +1,6 @@
+npm install -g sonar-scanner
+sonar-scanner \
+  -Dsonar.projectKey=react-devsecops \
+  -Dsonar.sources=src \
+  -Dsonar.host.url=http://<your-sonarqube-server> \
+  -Dsonar.login=$SONAR_TOKEN
