8997a9fdda35b47043d195e2bb7fd3aca1731f8f
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-24 10:34:23 +0530

    Update full-script-prom-client-garg.js

--- Prometheus and Grafana/full-script-prom-client-garg.js
+++ Prometheus and Grafana/full-script-prom-client-garg.js
@@ -22,4 +22,22 @@ scrape_configs:
     static_configs:
       - targets: ["<NDOEJS_SERVER_ADDRESS>"]  // If on AWS, instance id:port and on local, ifconfig pick up inet private ip and paste
 
-// Custom Metrics
+// Custom Metrics -- inside index.js
+npm i response-time
+
+// Start writing
+const responseTime = require('response-time')
+
+// Creating Histogram of this route
+const reqResTime = new client.Histogram({
+    name: 'http_req_res_time',
+    help: 'Request Response Time',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [1,50,100,200,500,800,1000,2000]  // buckets are used to categorize the time taken
+})
+
+// Custom Metrics Route
+// This will tell the requests take how much time
+app.use(responseTime((req, res, time) => {
+    reqResTime.labels({ method: req.method, route: req.url, code: req.statusCode }).observe(time)
+}))
