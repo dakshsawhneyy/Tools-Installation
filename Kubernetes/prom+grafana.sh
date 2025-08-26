85c06bc47f1aaec78fc7c2bb599d97212fc02e6d
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-26 09:55:27 +0530

    Update prom+grafana.sh

--- Kubernetes/prom+grafana.sh
+++ Kubernetes/prom+grafana.sh
@@ -1,8 +1,17 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
 helm repo update
+kubectl create namespace monitoring
+helm install kind-prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --set prometheus.service.nodePort=30000 --set prometheus.service.type=NodePort --set grafana.service.nodePort=31000 --set grafana.service.type=NodePort --set alertmanager.service.nodePort=32000 --set alertmanager.service.type=NodePort --set prometheus-node-exporter.service.nodePort=32001 --set prometheus-node-exporter.service.type=NodePort
+kubectl get svc -n monitoring
+kubectl get namespace
 
-# Install Prometheus
-helm install prometheus prometheus-community/prometheus
+# Expose these services
+kubectl port-forward svc/kind-prometheus-kube-prome-prometheus -n monitoring 9090:9090 --address=0.0.0.0 &
+kubectl port-forward svc/kind-prometheus-grafana -n monitoring 31000:80 --address=0.0.0.0 &
 
-# Install Grafana
-helm install grafana grafana/grafana
+# Prom Queries
+sum (rate (container_cpu_usage_seconds_total{namespace="default"}[1m])) / sum (machine_cpu_cores) * 100
+sum (container_memory_usage_bytes{namespace="default"}) by (pod)
+sum(rate(container_network_receive_bytes_total{namespace="default"}[5m])) by (pod)
+sum(rate(container_network_transmit_bytes_total{namespace="default"}[5m])) by (pod)
