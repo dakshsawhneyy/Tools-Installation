e0936962de81cf39e3038f3681a7c621216c7f0b
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-09-14 15:18:11 +0530

    Update argocd.sh

--- Kubernetes/argocd.sh
+++ Kubernetes/argocd.sh
@@ -5,7 +5,7 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # Wait for it to be ready (this can take a couple of minutes):
 kubectl wait --for=condition=ready pod --all -n argocd --timeout=5m
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443
-
 # Get the initial password by running this in your original terminal:
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
