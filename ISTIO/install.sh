73aea4c256f417977370c59ef4da4b79adc09904
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-09-15 11:19:24 +0530

    Update install.sh

--- ISTIO/install.sh
+++ ISTIO/install.sh
@@ -1,3 +1,4 @@
+# Step 1 - Download the Istio CLI (istioctl)
 curl -L https://istio.io/downloadIstio | sh -
 
 cd istio-1.23.0
@@ -6,3 +7,13 @@ cd istio-1.23.0
 export PATH=$PWD/bin:$PATH
 
 istioctl version
+
+# Step 2 - Install Istio on Your Kind Cluster
+istioctl install --set profile=demo -y
+
+# Step 3: Enable Automatic Sidecar Injection
+kubectl label namespace app istio-injection=enabled
+
+# Step 4: Relaunch Your Application to Trigger the Injection
+kubectl rollout restart deployment -n app
+kubectl get pods -n app
