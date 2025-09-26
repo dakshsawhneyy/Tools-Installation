c784eac545b7abdf821a954aa16a1dccecc9242c
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-09-26 14:36:12 +0530

    Update install.sh

--- Serverless_Framework/install.sh
+++ Serverless_Framework/install.sh
@@ -2,3 +2,6 @@ npm install -g serverless
 
 # Run the command to create a new service from the AWS Node.js template:
 serverless create --template aws-nodejs --path serverless_framework
+
+# For Canary Deployment -- deploymentSettings Block --
+npm install --save-dev serverless-plugin-canary-deployments
