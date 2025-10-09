3b0c7e25298fb4b6fba5d5ff5e0c49592763273d
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-10-09 17:07:24 +0530

    Create azurecli.sh

---
+++ AZURE/azurecli.sh
@@ -0,0 +1,26 @@
+# Option 1: Install with one command
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Option 2
+# Get packages needed for the installation process:
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Download and install the Microsoft signing key:
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+  gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+# Add the Azure CLI software repository:
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+
+# Update repository information and install the azure-cli package:
+sudo apt-get update
+sudo apt-get install azure-cli
