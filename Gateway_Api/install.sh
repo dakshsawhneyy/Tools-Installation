39a0db0e750e916868e82321233a5dc8581d6cb7
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-12-31 01:27:39 +0530

    Add GatewayClass definition to install.sh
    Added GatewayClass definition to install script.

--- Gateway_Api/install.sh
+++ Gateway_Api/install.sh
@@ -3,3 +3,11 @@ helm install eg oci://docker.io/envoyproxy/gateway-helm --version v1.6.1 -n envo
 
 # Check the status
 kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
+
+# Create gateway_class.yml
+apiVersion: gateway.networking.k8s.io/v1
+kind: GatewayClass
+metadata:
+  name: eg
+spec:
+  controllerName: gateway.envoyproxy.io/gatewayclass-controller
