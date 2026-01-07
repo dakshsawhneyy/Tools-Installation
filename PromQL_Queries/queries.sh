eaca83f2b01aec01982e23c6b5536b0cfa8dae89
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-01-07 17:02:24 +0530

    Add p95 latency query to queries.sh
    Add p95 latency query to PromQL script

--- PromQL_Queries/queries.sh
+++ PromQL_Queries/queries.sh
@@ -10,3 +10,9 @@ sum(rate(http_requests_total{status_code=~"5.."}[5m]))
 sum(rate(http_requests_total[5m]))
 
 
+# p95 Latency (seconds)
+histogram_quantile(
+  0.95,
+  sum(rate(http_request_duration_seconds_bucket[5m])) by (le)
+)
+
