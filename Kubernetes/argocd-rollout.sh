3e1b49a9c956696b2d35fa3fd72a088e324a4fe6
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-02-09 17:33:28 +0530

    Update Argo Rollout configuration for sample app

--- Kubernetes/argocd-rollout.sh
+++ Kubernetes/argocd-rollout.sh
@@ -8,36 +8,44 @@ curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kube
 chmod +x ./kubectl-argo-rollouts-linux-amd64
 sudo mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 
+---
 
 # Rollout
 apiVersion: argoproj.io/v1alpha1
 kind: Rollout
 metadata:
-  name: example-rollout
+  name: sample-rollout
+  namespace: default
 spec:
-  replicas: 10
+  replicas: 4
   selector:
     matchLabels:
-      app: nginx
+      app: sample-app
   template:
     metadata:
       labels:
-        app: nginx
+        app: sample-app
+        role: web
     spec:
       containers:
-        - name: nginx
-          image: nginx:1.15.4
+        - name: sample-app
+          image: dakshsawhneyy/helloworld-v1:latest
           ports:
-            - containerPort: 80
-  minReadySeconds: 30
-  revisionHistoryLimit: 3
+            - containerPort: 8000
+          resources:
+            requests:
+              cpu: "100m"
+              memory: "128Mi"
+            limits:
+              cpu: "500m"
+              memory: "512Mi"
   strategy:
     canary: #Indicates that the rollout should use the Canary strategy
-      maxSurge: '25%'
-      maxUnavailable: 0
+      # maxSurge: '25%'
+      # maxUnavailable: 0
       steps:
-        - setWeight: 10
+        - setWeight: 25
         - pause:
-            duration: 1h # 1 hour
-        - setWeight: 20
+            duration: 10m # wait for 10 mins
+        - setWeight: 50
         - pause: {} # pause indefinitely
