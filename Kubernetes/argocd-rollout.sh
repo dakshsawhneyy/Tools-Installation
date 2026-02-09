6ea98a74d56afbf36017e6086cca2f245a991a06
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-02-09 17:26:48 +0530

    Add installation steps for kubectl-argo-rollouts
    Added commands to download and install kubectl-argo-rollouts.

--- Kubernetes/argocd-rollout.sh
+++ Kubernetes/argocd-rollout.sh
@@ -4,6 +4,11 @@ kubectl create namespace argo-rollouts
 # Install the latest stable version
 kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
+curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-darwin-amd64
+chmod +x ./kubectl-argo-rollouts-darwin-amd64
+sudo mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
+
+
 # Rollout
 apiVersion: argoproj.io/v1alpha1
 kind: Rollout
