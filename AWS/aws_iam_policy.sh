14d46a915adfb19e9af226c5ace27dd43378d652
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-04-17 09:49:10 +0530

    Update aws_iam_policy.sh

--- AWS/aws_iam_policy.sh
+++ AWS/aws_iam_policy.sh
@@ -1,1 +1,17 @@
+# Download IAM Policy
 curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.11.0/docs/install/iam_policy.json
+
+# Create IAM Policy
+aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
+
+# Create IAM Role
+eksctl create iamserviceaccount \
+  --cluster=<your-cluster-name> \
+  --namespace=kube-system \
+  --name=aws-load-balancer-controller \
+  --role-name AmazonEKSLoadBalancerControllerRole \
+  --attach-policy-arn=arn:aws:iam::<your-aws-account-id>:policy/AWSLoadBalancerControllerIAMPolicy \
+  --approve
+
+
+  
