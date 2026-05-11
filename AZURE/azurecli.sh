2a3f4152451d0cee28098376bf3f86e5767ffa5e
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-05-11 22:55:09 +0530

    Update azurecli.sh

--- AZURE/azurecli.sh
+++ AZURE/azurecli.sh
@@ -36,7 +36,7 @@ az aks get-credentials \
   --resource-group aiopsplatform \
   --name aiopsplatform-AKS-cluster
 
-# Output
+# Outputgg
 output "Change_Config_Command" {
   value = "az aks get-credentials --resource-group ${azurerm_resource_group.demo.name} --name ${azurerm_kubernetes_cluster.demo.name}"
 }
